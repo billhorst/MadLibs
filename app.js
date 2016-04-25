eb15515663c4a3cae65746eae34c7c215fefce75
Bill Horst <billhorst7@gmail.com>
2016-04-24 22:36:39 -0600

    made editable

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var app = angular.module('madLibs', []);
-	
-	app.controller('mlCtrl', function($scope) {
-		$scope.active = {
-			fillingInWords: true,
+
+app.controller('mlCtrl', function($scope) {
+	$scope.active = {
+		fillingInWords: true,
 			// horrorStory: false,
 			// actionStory: false,
 			// sadStory: false,
@@ -11,42 +11,62 @@ var app = angular.module('madLibs', []);
 		// $scope.fillingInWords = true;
 
 		$scope.showHorror = function() {
+			$scope.formComplete = true;
 			// $scope.active.fillingInWords = false;
 			// $scope.active.actionStory = false;
 			// $scope.active.sadStory = false;
-			$scope.active = {};
-			$scope.active.horrorStory = true;
+			if($scope.madLibsForm.$valid) {
+				$scope.active = {};
+				$scope.active.horrorStory = true;
+			}
 		}
 
 		$scope.showAction = function() {
+			$scope.formComplete = true;
 			// $scope.active.fillingInWords = false;
 			// $scope.active.horrorStory = false;
 			// $scope.active.sadStory = false;
-			$scope.active = {};
-			$scope.active.actionStory = true;
+			if($scope.madLibsForm.$valid) {
+				$scope.active = {};
+				$scope.active.actionStory = true;
+			}
 		}
 
 		$scope.showSad = function() {
+			$scope.formComplete = true;
 			// $scope.active.fillingInWords = false;
 			// $scope.active.horrorStory = false;
 			// $scope.active.actionStory = false;
+			if($scope.madLibsForm.$valid) {
+				$scope.active = {};
+				$scope.active.sadStory = true;
+			}
+		}
+
+		$scope.editWordList = function() {
 			$scope.active = {};
-			$scope.active.sadStory = true;
+			$scope.active.fillingInWords = true;
 		}
+
+		// $scope.editWordListShowing = function() {
+		// 	$scope.active.horrorStory = true;
+		// 	$scope.active.fillingInWords = true;
+		// }
+
 	})
 
-	.directive('madLibsStory', function() {
-		return {
-			restrict: 'E',
-			transclude: true,
-			templateUrl: 'madLibsTemplate.html'
-		}
-	})
+.directive('madLibsStory', function() {
+	return {
+		restrict: 'E',
+		transclude: true,
+		templateUrl: 'madLibsTemplate.html'
+	}
+})
 
-	.directive('fillInWords', function() {
-		return {
-			restrict: 'E',
-			transclude: false,
-			templateUrl: 'wordList.html'
-		}
-	})
+.directive('fillInWords', function() {
+	return {
+		restrict: 'E',
+		transclude: false,
+		templateUrl: 'wordList.html'
+	}
+})
