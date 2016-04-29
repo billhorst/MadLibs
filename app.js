edb18fde88a88dd75fa0d0e9b0c9d81187234cbd
Bill Horst <billhorst7@gmail.com>
2016-04-28 23:03:45 -0600

    added random word generator

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var app = angular.module('madLibs', []);
 
 app.controller('mlCtrl', function($scope) {
+	$scope.word = {};
 	$scope.active = {
 		fillingInWords: true,
 			// horrorStory: false,
@@ -53,6 +54,18 @@ app.controller('mlCtrl', function($scope) {
 			$scope.active = {};
 			$scope.formComplete = false;
 			$scope.active.fillingInWords = true;
+		}
+
+		$scope.genRanWords = function() {
+			var propNameArray = ["Billy","Jimbo","Krzysztof","Vlad"];
+			var emotionArray = ["happy","angry","cheery","jaded"];
+			var verbArray = ["go","run","confront","cry"];
+			var animalArray = ["dog","cat","monkey","drop bear"];
+
+			$scope.word.properName = propNameArray[Math.floor(Math.random()*4)];
+			$scope.word.emotion = emotionArray[Math.floor(Math.random()*4)];
+			$scope.word.verb = verbArray[Math.floor(Math.random()*4)];
+			$scope.word.animal = animalArray[Math.floor(Math.random()*4)];
 		}
 
 		// $scope.editWordListShowing = function() {
