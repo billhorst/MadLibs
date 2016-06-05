37d511d7cacf0124bacc5d4c83e7009a96c50449
Bill Horst <billhorst7@gmail.com>
2016-06-04 22:49:46 -0600

    changed directives.js

--- scripts/directives.js
+++ scripts/directives.js
@@ -4,21 +4,21 @@ angular.module('directives', [])
 	return {
 		restrict: 'E',
 		transclude: false,
-		templateUrl: '../templates/wordList.html'
+		templateUrl: 'templates/wordList.html'
 	}
 })
 
 .directive('buttonList', function() {
 	return {
 		restrict: 'E',
-		templateUrl: '../templates/buttonList.html'
+		templateUrl: 'templates/buttonList.html'
 	}
 })
 
 .directive('storiesView', function() {
 	return {
 		restrict: 'E',
-		templateUrl: '../templates/stories.html'
+		templateUrl: 'templates/stories.html'
 	}
 })
 
@@ -26,20 +26,20 @@ angular.module('directives', [])
 	return {
 		restrict: 'E',
 		transclude: true,
-		templateUrl: '../templates/inputBox.html'
+		templateUrl: 'templates/inputBox.html'
 	}
 })
 
 .directive('introToApp', function() {
 	return {
 		restrict: 'E',
-		templateUrl: '../templates/intro.html'
+		templateUrl: 'templates/intro.html'
 	}
 })
 
 .directive('foot', function() {
 	return {
 		restrict: 'E',
-		templateUrl: '../templates/footer.html'
+		templateUrl: 'templates/footer.html'
 	}
 })
